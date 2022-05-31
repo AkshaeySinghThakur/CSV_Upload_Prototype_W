e4efd6d5b511cbfc12635b94e5ef6ab05d094906
Akshaey Singh Thakur <64556767+AkshaeySinghThakur@users.noreply.github.com>
2022-05-31 17:17:10 +0530

    Add files via upload
    Prototype W

---
+++ models/file.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose") ;
+const {Schema} = mongoose ;
+
+
+// creating schema for file 
+const fileSchema = new Schema ( {
+    name:{
+        type:String,
+        required:true,
+    },
+    data: [{
+        type: Object,
+        required:true
+    }]
+},
+{
+    timestamps:true 
+}) ;
+
+const Files = mongoose.model("Files",fileSchema);
+
+module.exports = Files ;
